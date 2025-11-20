efb24a09fd09369c6c1f6cead04dd4ff4c60fa86
pedro-rodrigues2025 <pedroalr2005@gmail.com>
2025-11-20 10:43:10 -0300

    criação das páginas de login e cadastro, criação da página da dashbord, início do back-end, criação da página do quiz

--- web-data-viz/src/models/aquarioModel.js
+++ web-data-viz/src/models/aquarioModel.js
@@ -1,23 +1,48 @@
 var database = require("../database/config");
 
-function buscarAquariosPorEmpresa(empresaId) {
-
-  var instrucaoSql = `SELECT * FROM aquario a WHERE fk_empresa = ${empresaId}`;
+function buscarCadastroPorLogin(idLogin){
+  var instrucaoSql = `SELECT * FROM login a WHERE fk = ${idCadastro}`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
 
-function cadastrar(empresaId, descricao) {
+
+function cadastro(idCadastro, nome, emial, senha) {
   
-  var instrucaoSql = `INSERT INTO (descricao, fk_empresa) aquario VALUES (${descricao}, ${empresaId})`;
+  var instrucaoSql = `INSERT INTO (nome, email, senha) cadastro VALUES (${nome}, ${email}, ${senha})`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
-
 
 module.exports = {
-  buscarAquariosPorEmpresa,
-  cadastrar
+  buscarCadastroPorLogin,
+  cadastro
 }
+
+
+
+
+
+// function buscarAquariosPorEmpresa(empresaId) {
+
+//   var instrucaoSql = `SELECT * FROM aquario a WHERE fk_empresa = ${empresaId}`;
+
+//   console.log("Executando a instrução SQL: \n" + instrucaoSql);
+//   return database.executar(instrucaoSql);
+// }
+
+// function cadastrar(empresaId, descricao) {
+  
+//   var instrucaoSql = `INSERT INTO (descricao, fk_empresa) aquario VALUES (${descricao}, ${empresaId})`;
+
+//   console.log("Executando a instrução SQL: \n" + instrucaoSql);
+//   return database.executar(instrucaoSql);
+// }
+
+
+// module.exports = {
+//   buscarAquariosPorEmpresa,
+//   cadastrar
+// }
